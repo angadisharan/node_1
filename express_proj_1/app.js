3b833a036a3310e6ff92692dd8a197cfcbdbe4ee
Ganapati Hegde <ksganapatihegde@gmail.com>
2017-06-28 17:50:46 +0530

    passport

--- express_proj_1/app.js
+++ express_proj_1/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 
 
+var expressSession = require('express-session')
 
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -19,6 +20,8 @@ var handle_request = require('./routes/handle_request');
 
 
 //Handlers
+var db_utils = require('./db/db_utils')
+
 var store_item_add = require('./routes/store_item/store_item_add')
 var store_item_getall = require('./routes/store_item/store_item_getall')
 var store_item_update = require('./routes/store_item/store_item_update')
@@ -33,6 +36,8 @@ var app = express();
 console.log("__dirname " + __dirname);
 
 
+app.use(cookieParser());
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -41,11 +46,164 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(expressSession({secret: 'someSecret', resave:false, saveUninitialize:false, 
+  cookie: {maxAge: 3000000}})
+)
+
+//=================================== Passport ==========================================
+
+
+app.get('/new_login', function(req, res){
+  res.render('new_login')
+})
+
+
+// app.use(function(req, res, next){
+
+//   console.log("=================== inside middleware")
+//   next();
+// });
+
+
+var passport = require('passport');
+var passportLocal = require('passport-local');
+
+passport.use(new passportLocal.Strategy(
+  {passReqToCallback: true},
+  verifyCredentials
+));
+
+
+
+function verifyCredentials(req, username, password, done) {
+  
+    var info = { message: 'Invalid User name or password', 'username':username, 'password':password }
+    var sql = " select * from user where email = '" + username + "' "
+    
+    console.log(" inside verifyCredentials " + username + password)
+
+    db_utils.executeSQL(sql, function(result) {
+
+
+      console.log(" result ==== inside verifyCredentials " + JSON.stringify(result))
+
+      if(!result || result.length <= 0) {
+        done(null, null, info);
+      } else {
+        if(result[0].password == password){
+          var user = {
+            'user_id': result[0].user_id
+          }
+          done(null, user);
+        } else {
+          done(null, null, info);
+        }
+      }
+    });
+}
+
+
+passport.serializeUser(function(user, done) {
+
+    console.log("inside serializeUser")
+
+    var sid_user = {
+      'user_id':user.user_id
+    };
+    done(null, sid_user);
+});
+
+
+passport.deserializeUser(function(sid_user, done) {
+
+    console.log("inside deserializeUser")
+
+    var sql = " select * from user where usre_id = '" + sid_user.user_id + "' "
+    db_utils.executeSQL(sql, function(result) {
+      if(result.length <= 0){
+        done(null, null, { message: 'Invalid User name or password' });
+      } else {
+        done(null, result[0]);
+      }
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+  app.post('/new_post_login', function(req, res, next) {
+
+    console.log("inside post newLogin")
+
+    passport.authenticate('local', function(err, user, info) {
+      
+
+      console.log(" user ==== iinside post " + JSON.stringify(user));
+      console.log(" user isAuthenticated " + req.isAuthenticated());
+
+
+
+      if (err) { return next(err) }
+      if (!user) {
+
+
+
+        return res.render('login', info);
+      }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+          res.redirect("/");
+      });
+    })(req, res, next);
+
+
+
+  });
+
+module.exports.isAuthenticated = isAuthenticated;
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+       return next();
+    } else {
+      return utils.sendWithStatus(res, 401, "Unauthorised");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=================================== Passport ==========================================
+
+
+
+
+
+
+
+
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -62,6 +220,13 @@ app.get("/login", handle_request.login);
 
 app.get("/signup", handle_request.render_signup);
 app.post("/api/signup", handle_request.perform_signup);
+
+
+
+
+
+app.get('/api/admin/item/getall', store_item_getall.handleRequest)
+
 
 
 app.post("/post_login", handle_request.handle_login);
@@ -130,6 +295,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
 
 
 module.exports = app;
